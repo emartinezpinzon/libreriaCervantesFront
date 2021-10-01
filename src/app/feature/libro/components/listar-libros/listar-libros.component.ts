e275c1eb09a26df87471a63e56c6c26b8da9b404
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 05:11:52 -0500

    Agrega el servicio para listar libros

--- src/app/feature/libro/components/listar-libros/listar-libros.component.ts
+++ src/app/feature/libro/components/listar-libros/listar-libros.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Libro } from '@libro/shared/model/libro';
+import { ListarLibrosService } from '@libro/shared/service/listar-libros.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-listar-libros',
   templateUrl: './listar-libros.component.html',
   styleUrls: ['./listar-libros.component.css']
 })
+
 export class ListarLibrosComponent implements OnInit {
+  public listaLibros: Observable<Libro[]>;
 
-  constructor() { }
+  constructor(protected libroService: ListarLibrosService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.listaLibros = this.libroService.consultar();
   }
 
 }
---
+++ src/app/feature/libro/shared/service/listar-libros.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { HttpService } from "@core/services/http.service";
+import { environment } from "src/environments/environment";
+import { Libro } from "../model/libro";
+
+@Injectable()
+export class ListarLibrosService {
+
+    constructor(protected http: HttpService) {}
+
+    public consultar() {
+        return this.http.doGet<Libro[]>(`${environment.endpoint}/libro`,
+        this.http.optsName('listar libros'));
+    }
+}
