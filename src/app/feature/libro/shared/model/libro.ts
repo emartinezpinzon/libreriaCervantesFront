8b9abd3938bb9c867159f4567707d1302261ef60
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 00:30:16 -0500

    Modelo de libro

---
+++ src/app/feature/libro/shared/model/libro.ts
@@ -0,0 +1,17 @@
+export class Compra {
+    id: number;
+    titulo: string;
+    categoria: string;
+    distribucion: string;
+    disponibles: number;
+    precio: number;
+
+    constructor(id: number, titulo: string, categoria: string, distribucion: string, disponibles: number, precio: number) {
+        this.id = id;
+        this.titulo = titulo;
+        this.categoria = categoria;
+        this.distribucion = distribucion;
+        this.disponibles = disponibles;
+        this.precio = precio;
+    }
+}
