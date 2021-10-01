a250086bc034b25bbeb9ce70fb2050af6d8e19a8
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-30 23:50:50 -0500

    Agrega path completo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  endpoint: '/promociones'
+  endpoint: 'http://localhost:8080/promociones'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: '/promociones'
+  endpoint: 'http://localhost:8080/promociones'
 };
 
 /*
