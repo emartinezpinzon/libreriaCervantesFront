8f93f9d8f37a5fb59924727368e3ab2d50ea8c31
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 01:32:28 -0500

    Cambia color de toolbar

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-toolbar',
   templateUrl: 'toolbar.component.html',
   styles: [`:host {
-    background-color: #106cc8;
+    background-color: #006770;
     color: rgba(255, 255, 255, 0.87);
     display: block;
     height: 48px;
