a56efa2e122beb02b5b05799e64fc3ac5106c582
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 00:27:37 -0500

    Ficheros para libro creados (aún vacíos)

---
+++ src/app/feature/libro/components/crear-libros/crear-libros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-libros',
+  templateUrl: './crear-libros.component.html',
+  styleUrls: ['./crear-libros.component.css']
+})
+export class CrearLibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
