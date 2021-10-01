3489f811ffc6751a272ea5feed5287be1cb824fb
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 18:13:28 -0500

    Crear libro montado sin revisi´n

--- src/app/feature/libro/components/crear-libros/crear-libros.component.ts
+++ src/app/feature/libro/components/crear-libros/crear-libros.component.ts
@@ -1,4 +1,12 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Libro } from '@libro/shared/model/libro';
+import { delay, tap } from 'rxjs/operators';
+import { LibroService } from '@libro/shared/service/libro.service';
+
+const CANTIDAD_MINIMA_LIBROS = 10;
+const PRECIO_MINIMO_LIBROS = 999;
 
 @Component({
   selector: 'app-crear-libros',
@@ -6,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-libros.component.css']
 })
 export class CrearLibrosComponent implements OnInit {
+  libroForm: FormGroup;
+  libro = {} as Libro;
 
-  constructor() { }
+  constructor(protected libroService: LibroService, private router: Router) { }
 
   ngOnInit(): void {
+    this.construirFormularioLibro();
+  }
+
+  onSubmit() {
+    this.libroService.guardar(this.libroForm.value)
+    .pipe(
+      tap(() => this.router.navigate(['libro'])),
+      delay(500)
+    )
+    .subscribe(
+      data => {console.log(data)},
+      error => {console.log(error)}
+    );
+  }
+
+  private construirFormularioLibro() {
+    this.libroForm = new FormGroup({
+      titulo: new FormControl('', Validators.required),
+      categoria: new FormControl(''),
+      distribucion: new FormControl(''),
+      disponibles: new FormControl('', Validators.min(CANTIDAD_MINIMA_LIBROS)),
+      precio: new FormControl('', Validators.min(PRECIO_MINIMO_LIBROS))
+    });
   }
 }
---
+++ src/app/feature/libro/shared/service/crear-libros.service.ts
@@ -0,0 +1,15 @@
+import { environment } from "src/environments/environment";
+import { Libro } from "../model/libro";
+import { HttpService } from "@core/services/http.service";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class CrearLibrosService {
+    
+    constructor(protected http: HttpService) { }
+
+    public guardar(libro: Libro) {
+        return this.http.doPost<Libro, boolean>(`${environment.endpoint}/libros`, libro,
+                                                this.http.optsName('crear/actualizar libro'));
+    }
+}
