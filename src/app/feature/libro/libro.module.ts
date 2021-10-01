f518c8ff4c149d20dcea6354f49cf8c19164df7a
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 00:26:56 -0500

    Agrega libro(& routing) component

---
+++ src/app/feature/libro/libro.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { LibroComponent } from "@libro/components/libro/libro.component";
+import { ListarLibrosComponent } from "@libro/components/listar-libros/listar-libros.component";
+import { CrearLibrosComponent } from "@libro/components/crear-libros/crear-libros.component";
+import { LibroRoutingModule } from "@libro/libro-routing.module";
+import { SharedModule } from "@shared/shared.module";
+
+
+@NgModule({
+  declarations: [
+    LibroComponent,
+    ListarLibrosComponent,
+    CrearLibrosComponent
+  ],
+  imports: [
+      LibroRoutingModule,
+      SharedModule
+  ],
+  providers: [LibroService]
+})
+export class LibroModule {}
