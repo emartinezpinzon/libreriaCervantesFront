dd32627ac9d226aa736105f5d4cfb8c2cfc3a13f
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-02 16:40:36 -0500

    Comunicación entre componentes padre e hijo

--- src/app/feature/compra/components/crear-compra/crear-compra.component.ts
+++ src/app/feature/compra/components/crear-compra/crear-compra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crear-compra',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCompraComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  libroId: number;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
