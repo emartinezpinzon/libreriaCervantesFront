7e6dcdeea2a793bdc77d5bd1955418412035c4a9
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-02 11:12:31 -0500

    Remueve producto del menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ export class AppComponent {
   title = 'app-base';
 
   public companies: MenuItem[] = [
-    { url: '/producto', nombre: 'producto' },
     { url: '/libro', nombre: 'Libros' },
     { url: '/compra', nombre: 'Compra' }
   ];
