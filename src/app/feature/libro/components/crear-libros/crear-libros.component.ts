3a28b725427b2491f91d4f1d61501a4acf7c9474
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-01 23:46:51 -0500

    Formato de texto para libro

--- src/app/feature/libro/components/crear-libros/crear-libros.component.ts
+++ src/app/feature/libro/components/crear-libros/crear-libros.component.ts
@@ -1,47 +1,52 @@
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Libro } from '@libro/shared/model/libro';
-import { delay, tap } from 'rxjs/operators';
-import { LibroService } from '@libro/shared/service/libro.service';
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Libro } from "@libro/shared/model/libro";
+import { delay, tap } from "rxjs/operators";
+import { LibroService } from "@libro/shared/service/libro.service";
 
 const CANTIDAD_MINIMA_LIBROS = 10;
 const PRECIO_MINIMO_LIBROS = 999;
 
 @Component({
-  selector: 'app-crear-libros',
-  templateUrl: './crear-libros.component.html',
-  styleUrls: ['./crear-libros.component.css']
+  selector: "app-crear-libros",
+  templateUrl: "./crear-libros.component.html",
+  styleUrls: ["./crear-libros.component.css"],
 })
 export class CrearLibrosComponent implements OnInit {
   libroForm: FormGroup;
   libro = {} as Libro;
 
-  constructor(protected libroService: LibroService, private router: Router) { }
+  constructor(protected libroService: LibroService, private router: Router) {}
 
   ngOnInit(): void {
     this.construirFormularioLibro();
   }
 
   onSubmit() {
-    this.libroService.guardar(this.libroForm.value)
-    .pipe(
-      tap(() => this.router.navigate(['libro'])),
-      delay(500)
-    )
-    .subscribe(
-      data => {console.log(data)},
-      error => {console.log(error)}
-    );
+    this.libroService
+      .guardar(this.libroForm.value)
+      .pipe(
+        tap(() => this.router.navigate(["libro"])),
+        delay(500)
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
   private construirFormularioLibro() {
     this.libroForm = new FormGroup({
-      titulo: new FormControl('', Validators.required),
-      categoria: new FormControl(''),
-      distribucion: new FormControl(''),
-      disponibles: new FormControl('', Validators.min(CANTIDAD_MINIMA_LIBROS)),
-      precio: new FormControl('', Validators.min(PRECIO_MINIMO_LIBROS))
+      titulo: new FormControl("", Validators.required),
+      categoria: new FormControl(""),
+      distribucion: new FormControl(""),
+      disponibles: new FormControl("", Validators.min(CANTIDAD_MINIMA_LIBROS)),
+      precio: new FormControl("", Validators.min(PRECIO_MINIMO_LIBROS)),
     });
   }
 }
--- src/app/feature/libro/components/listar-libros/listar-libros.component.ts
+++ src/app/feature/libro/components/listar-libros/listar-libros.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Libro } from '@libro/shared/model/libro';
-import { LibroService } from '@libro/shared/service/libro.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Libro } from "@libro/shared/model/libro";
+import { LibroService } from "@libro/shared/service/libro.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-listar-libros',
-  templateUrl: './listar-libros.component.html',
-  styleUrls: ['./listar-libros.component.css']
+  selector: "app-listar-libros",
+  templateUrl: "./listar-libros.component.html",
+  styleUrls: ["./listar-libros.component.css"],
 })
-
 export class ListarLibrosComponent implements OnInit {
   public listaLibros: Observable<Libro[]>;
 
-  constructor(protected libroService: LibroService) { }
+  constructor(protected libroService: LibroService) {}
 
   ngOnInit() {
     this.listaLibros = this.libroService.consultar();
