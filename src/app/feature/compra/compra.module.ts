185b7fcb5771dfa34e1b0dbc0f805f893fc5cb51
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 03:36:00 -0500

    Agrega el componente de crear factura en compra

--- src/app/feature/compra/compra.module.ts
+++ src/app/feature/compra/compra.module.ts
@@ -5,11 +5,13 @@ import { SharedModule } from '@shared/shared.module';
 import { CompraService } from './shared/service/compra.service';
 import { CompraComponent } from './components/compra/compra.component';
 import { ListarCompraComponent } from './components/listar-compra/listar-compra.component';
+import { CrearFacturaComponent } from '@factura/components/crear-factura/crear-factura.component';
 
 @NgModule({
   declarations: [
     CompraComponent,
-    ListarCompraComponent
+    ListarCompraComponent,
+    CrearFacturaComponent
   ],
   imports: [
     CompraRoutingModule,
