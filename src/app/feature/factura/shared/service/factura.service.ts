2a3973e222f0e5756e8212a1c9553dd689b3e11d
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 03:29:13 -0500

    Agrega el servicio de factura

---
+++ src/app/feature/factura/shared/service/factura.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpService } from "@core/services/http.service";
+import { environment } from "src/environments/environment";
+import { Factura } from "../model/factura";
+
+@Injectable()
+export class FacturaService {
+
+    constructor(protected http: HttpService) {}
+
+    public consultarById(id: number) {
+        return this.http.doGet<Factura>(`${environment.endpoint}/compra/${id}`,
+                                                    this.http.optsName('listar compras'));
+    }
+
+    public guardar(factura: Factura) {
+        return this.http.doPost<Factura, boolean>(`${environment.endpoint}/factura`, factura,
+                                                    this.http.optsName('crear factura'));
+    }
+}
