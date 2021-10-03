7e210e29f3e161a7f697aa1771a81b23b552f831
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 05:03:17 -0500

    Falta mostrar los datos traídos por el Observable<Factura>

--- src/app/feature/factura/components/ver-factura/ver-factura.component.ts
+++ src/app/feature/factura/components/ver-factura/ver-factura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Factura } from '@factura/shared/model/factura';
+import { FacturaService } from '@factura/shared/service/factura.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-ver-factura',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-factura.component.css']
 })
 export class VerFacturaComponent implements OnInit {
+  public factura: Observable<Factura>;
 
-  constructor() { }
+  constructor(protected facturaService: FacturaService) { }
 
   ngOnInit() {
+    let x = localStorage.getItem("facturaId");
+    let facturaId = +x;
+
+    this.factura = this.facturaService.consultarById(facturaId);
   }
 
 }
