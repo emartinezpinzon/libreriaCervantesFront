4c7655c6263e9f5e954a637bb55f51dc98fc03e4
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 04:46:52 -0500

    Creación de factura

--- src/app/feature/factura/components/crear-factura/crear-factura.component.ts
+++ src/app/feature/factura/components/crear-factura/crear-factura.component.ts
@@ -1,4 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Factura } from '@factura/shared/model/factura';
+import { FacturaService } from '@factura/shared/service/factura.service';
+import { delay, tap } from 'rxjs/operators';
+
+const REGISTRO_EXITOSO = "La factura se ha generado con éxito";
 
 @Component({
   selector: 'app-crear-factura',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearFacturaComponent implements OnInit {
 
-  constructor() { }
+  factura = {} as Factura;
 
-  ngOnInit() {
+  constructor(protected facturaService: FacturaService, private router: Router) { }
+
+  ngOnInit() { }
+
+  onSubmit() {
+    this.facturaService.guardar(this.factura)
+    .pipe(
+      tap(() => this.router.navigate(["factura"])),
+      delay(500)
+    )
+    .subscribe(
+      (data) => {
+        alert(REGISTRO_EXITOSO);
+        console.log(data);
+        localStorage.setItem("facturaId", data["valor"]);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.error.mensaje);
+      }
+    )
   }
 
 }
