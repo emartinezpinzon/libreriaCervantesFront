5a7a6be89cb37f31ac9af8ed8dbc239fbe0db23d
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 00:43:06 -0500

    Encapsula el botón de crear factura en su propio componente

--- src/app/feature/compra/compra.module.ts
+++ src/app/feature/compra/compra.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 
 import { CompraRoutingModule } from './compra-routing.module';
 import { SharedModule } from '@shared/shared.module';
-import { CompraService } from './shared/service/compra.service';
-import { CompraComponent } from './components/compra/compra.component';
-import { ListarCompraComponent } from './components/listar-compra/listar-compra.component';
+import { CompraService } from '@compra/shared/service/compra.service';
+import { CompraComponent } from '@compra/components/compra/compra.component';
+import { ListarCompraComponent } from '@compra/components/listar-compra/listar-compra.component';
+import { CrearFacturaComponent } from '@factura/components/crear-factura/crear-factura.component';
+import { FacturaService } from '@factura/shared/service/factura.service';
 
 @NgModule({
   declarations: [
     CompraComponent,
-    ListarCompraComponent
+    ListarCompraComponent,
+    CrearFacturaComponent
   ],
   imports: [
     CompraRoutingModule,
     SharedModule
   ],
-  providers: [CompraService]
+  providers: [CompraService, FacturaService]
 })
 export class CompraModule { }
