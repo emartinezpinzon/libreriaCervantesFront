178d3de649e7f944e708f17362c63838fa6a7004
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 03:17:34 -0500

    Agrega factura al path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SecurityGuard } from '@core/guard/security.guard';
+import { FacturaComponent } from '@factura/components/factura/factura.component';
 import { HomeComponent } from '@home/home.component';
 import { CrearLibrosComponent } from '@libro/components/crear-libros/crear-libros.component';
 
@@ -11,8 +12,8 @@ const routes: Routes = [
   { path: 'producto', loadChildren: () => import('@producto/producto.module').then(mod => mod.ProductoModule)},
   { path: 'libro', loadChildren: () => import('@libro/libro.module').then(mod => mod.LibroModule)},
   { path: 'crear-libro', component: CrearLibrosComponent},
-  { path: 'compra', loadChildren: () => import('./feature/compra/compra.module').then(mod => mod.CompraModule)}
-
+  { path: 'compra', loadChildren: () => import('./feature/compra/compra.module').then(mod => mod.CompraModule)},
+  { path: 'factura', component: FacturaComponent}
 ];
 
 @NgModule({
