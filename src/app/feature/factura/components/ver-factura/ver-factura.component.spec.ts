4f2e267be960b594d784e609a2e6f543ab0c9204
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-03 03:17:00 -0500

    Agrega ver factura

---
+++ src/app/feature/factura/components/ver-factura/ver-factura.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { VerFacturaComponent } from './ver-factura.component';
+
+describe('VerFacturaComponent', () => {
+  let component: VerFacturaComponent;
+  let fixture: ComponentFixture<VerFacturaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VerFacturaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VerFacturaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
