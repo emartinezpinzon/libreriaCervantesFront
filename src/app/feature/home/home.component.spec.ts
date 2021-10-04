ec28564a7399bde88fffb5693612b25126dbbd7f
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-04 03:49:37 -0500

    HomeComponent Test

--- src/app/feature/home/home.component.spec.ts
+++ src/app/feature/home/home.component.spec.ts
@@ -19,7 +19,7 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debe crearse el componente', () => {
     expect(component).toBeTruthy();
   });
 });
