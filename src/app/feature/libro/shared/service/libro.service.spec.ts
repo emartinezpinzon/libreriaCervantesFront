5197c5262acc28381da8003aacb7b9d2774c30ea
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-04 04:10:02 -0500

    Mejoras sintacticas en LibroService test

--- src/app/feature/libro/shared/service/libro.service.spec.ts
+++ src/app/feature/libro/shared/service/libro.service.spec.ts
@@ -1,53 +1,67 @@
-import { HttpResponse } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { HttpService } from '@core/services/http.service';
-import { environment } from 'src/environments/environment';
-import { Libro } from '../model/libro';
-import { LibroService } from './libro.service';
+import { HttpResponse } from "@angular/common/http";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { HttpService } from "@core/services/http.service";
+import { environment } from "src/environments/environment";
+import { Libro } from "../model/libro";
+import { LibroService } from "./libro.service";
 
-describe('LibroService', () => {
-    let libroServicio: LibroService;
-    let libroServicioMock: HttpTestingController;
+describe("LibroService", () => {
+  let libroServicio: LibroService;
+  let libroServicioMock: HttpTestingController;
 
-    beforeEach(() => {
-        const injector = TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
-            providers: [LibroService, HttpService]
-        });
-        libroServicioMock = injector.inject(HttpTestingController);
-        libroServicio = TestBed.inject(LibroService);
+  beforeEach(() => {
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LibroService, HttpService],
+    });
+    libroServicioMock = injector.inject(HttpTestingController);
+    libroServicio = TestBed.inject(LibroService);
+  });
+
+  it("Debe crear el servicio", () => {
+    expect(libroServicio).toBeTruthy();
+  });
+
+  it("Debe crear un libro", () => {
+    let dummyLibro: Libro = new Libro(
+      1,
+      "Ficciones",
+      "Literatura",
+      "Nacional",
+      3,
+      10
+    );
+
+    libroServicio.guardar(dummyLibro).subscribe((respuesta) => {
+      expect(respuesta).toEqual(true);
     });
 
-    it('Debe crear el servicio', () => {
-        expect(libroServicio).toBeTruthy();
+    const request = libroServicioMock.expectOne(
+      `${environment.endpoint}/libros`
+    );
+    expect(request.request.method).toBe("POST");
+    request.event(new HttpResponse<boolean>({ body: true }));
+  });
+
+  it("Debe consultar los libros creados", () => {
+    let dummyLibros: Libro[] = [
+      new Libro(1, "Ficciones", "Literatura", "Nacional", 3, 10),
+    ];
+    let cantidadLibros = dummyLibros.length;
+
+    libroServicio.consultar().subscribe(libros => {
+      expect(libros.length).toBe(cantidadLibros);
+      expect(libros).toEqual(dummyLibros);
     });
 
-    it('Debe crear un libro', () => {
-        let dummyLibro: Libro = new Libro(1, "Ficciones", "Literatura", "Nacional", 3, 10);
-
-        libroServicio.guardar(dummyLibro).subscribe(
-            (respuesta) => {
-                expect(respuesta).toEqual(true);
-            }
-        );
-
-        const req = libroServicioMock.expectOne(`${environment.endpoint}/libros`);
-        expect(req.request.method).toBe('POST');
-        req.event(new HttpResponse<boolean>({body: true}));
-    });
-
-    it('Debe consultar los libros creados', () => {
-        let cantidadLibros = 1;
-        let dummyLibros: Libro[] = [new Libro(1, "Ficciones", "Literatura", "Nacional", 3, 10)];
-
-        libroServicio.consultar().subscribe((libros) => {
-            expect(libros.length).toBe(cantidadLibros);
-            expect(libros).toEqual(dummyLibros);
-        });
-
-        const request = libroServicioMock.expectOne(`${environment.endpoint}/libros`);
-        expect(request.request.method).toBe('GET');
-        request.flush(dummyLibros);
-    });
+    const request = libroServicioMock.expectOne(
+      `${environment.endpoint}/libros`
+    );
+    expect(request.request.method).toBe("GET");
+    request.flush(dummyLibros);
+  });
 });
