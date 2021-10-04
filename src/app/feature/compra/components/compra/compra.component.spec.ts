32676cfdda37738d48d57eb9321a1cb35ad3060d
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-04 06:05:48 -0500

    CompraComponent Test Completo

--- src/app/feature/compra/components/compra/compra.component.spec.ts
+++ src/app/feature/compra/components/compra/compra.component.spec.ts
@@ -1,7 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompraComponent } from './compra.component';
 
@@ -9,12 +7,12 @@ describe('CompraComponent', () => {
   let component: CompraComponent;
   let fixture: ComponentFixture<CompraComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ CompraComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CompraComponent);
@@ -22,7 +20,7 @@ describe('CompraComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debe crearse el componente', () => {
     expect(component).toBeTruthy();
   });
 });
