5b723daa8e99b65ab1593e7d38e64b6e8a90a21a
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-05 14:45:07 -0500

    Quita producto del navbar

--- e2e/src/page/navbar/navbar.po.ts
+++ e2e/src/page/navbar/navbar.po.ts
@@ -2,13 +2,8 @@ import { by, element } from 'protractor';
 
 export class NavbarPage {
     linkHome = element(by.xpath('/html/body/app-root/app-navbar/nav/div/a[1]'));
-    linkProducto = element(by.xpath('/html/body/app-root/app-navbar/nav/a[2]'));
     linkLibro = element(by.xpath('/html/body/app-root/app-navbar/nav/div/div/ul/li[1]/a'));
     linkCompra = element(by.xpath('/html/body/app-root/app-navbar/nav/div/div/ul/li[2]/a'));
-
-    async clickBotonProductos() {
-        await this.linkProducto.click();
-    }
 
     async clickBotonLibro() {
         await this.linkLibro.click();
