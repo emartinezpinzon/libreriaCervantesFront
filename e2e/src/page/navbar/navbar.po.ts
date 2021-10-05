17a9b2b31448d34112c7ba2adc5a837d6415bfc1
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-05 14:16:05 -0500

    Agrega los botones de libro y compra al navbar

--- e2e/src/page/navbar/navbar.po.ts
+++ e2e/src/page/navbar/navbar.po.ts
@@ -1,10 +1,20 @@
 import { by, element } from 'protractor';
 
 export class NavbarPage {
-    linkHome = element(by.xpath('/html/body/app-root/app-navbar/nav/a[1]'));
+    linkHome = element(by.xpath('/html/body/app-root/app-navbar/nav/div/a[1]'));
     linkProducto = element(by.xpath('/html/body/app-root/app-navbar/nav/a[2]'));
+    linkLibro = element(by.xpath('/html/body/app-root/app-navbar/nav/div/div/ul/li[1]/a'));
+    linkCompra = element(by.xpath('/html/body/app-root/app-navbar/nav/div/div/ul/li[2]/a'));
 
     async clickBotonProductos() {
         await this.linkProducto.click();
+    }
+
+    async clickBotonLibro() {
+        await this.linkLibro.click();
+    }
+
+    async clickBotonCompra() {
+        await this.linkCompra.click();
     }
 }
