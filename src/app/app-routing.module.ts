63b19652007411aa238951c83fc7236f3e52146b
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-05 14:19:17 -0500

    Agrega ruta para factura/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'libro', loadChildren: () => import('@libro/libro.module').then(mod => mod.LibroModule)},
   { path: 'crear-libro', component: CrearLibrosComponent},
   { path: 'compra', loadChildren: () => import('@compra/compra.module').then(mod => mod.CompraModule)},
-  { path: 'factura', loadChildren: () => import('@factura/factura.module').then(mod => mod.FacturaModule)}
+  { path: 'factura/:id', loadChildren: () => import('@factura/factura.module').then(mod => mod.FacturaModule)}
 ];
 
 @NgModule({
