c43bc4e6836caf79ea532dcf25257bd565f32513
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-10-05 19:19:53 -0500

    Pruebas e2e con mejoras disponibles

--- e2e/src/test/libro.e2e-spec.ts
+++ e2e/src/test/libro.e2e-spec.ts
@@ -3,16 +3,19 @@ import { AppPage } from '../app.po';
 import { LibroPage } from '../page/libro/libro.po';
 import { randomUUID } from 'crypto';
 import { browser } from 'protractor';
+import { CompraPage } from '../page/compra/compra.po';
 
 describe('workspace-project Libro', () => {
     let page: AppPage;
     let navBar: NavbarPage;
     let libro: LibroPage;
+    let compra: CompraPage;
 
     beforeEach(() => {
         page = new AppPage();
         navBar = new NavbarPage();
         libro = new LibroPage();
+        compra = new CompraPage();
     });
 
     it('Deberia crear libro', () => {
@@ -33,7 +36,7 @@ describe('workspace-project Libro', () => {
         libro.ingresarPrecio(PRECIO);
 
         libro.clickBotonGuardarNuevoLibro();
-        browser.sleep(2000);
+        browser.sleep(2000); // Buscar sobre await
 
         browser.switchTo().alert().accept();
     });
@@ -56,5 +59,12 @@ describe('workspace-project Libro', () => {
         libro.ingresarCantidad(CANTIDAD_COMPRA);
 
         libro.clickBotonGuardarNuevaCompra();
+    });
+
+    it('Deberia crear factura', () => {
+        page.navigateTo();
+        navBar.clickBotonCompra();
+        browser.sleep(2000);
+        compra.clickBotonCrearFactura();
     });
 });
